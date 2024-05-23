666c916827062b18296da641bdbf1317c4b290c1
Marcos Felipe <0709.marcoscouto@gmail.com>
2024-05-22 21:31:42 -0300

    :sparkles: feat: create sign up component

---
+++ src/app/pages/signup/signup.component.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
+import { FormGroup, FormControl, ReactiveFormsModule, Validators, Form } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
+import { Router } from '@angular/router';
+import { LoginService } from '../../services/login.service';
+import { ToastrService } from 'ngx-toastr';
+
+interface SignupForm {
+  name: FormControl,
+  email: FormControl,
+  password: FormControl,
+  passwordConfirm: FormControl
+}
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [
+    DefaultLoginLayoutComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
+  ],
+  providers: [
+    LoginService
+  ],
+  templateUrl: './signup.component.html',
+  styleUrl: './signup.component.scss'
+})
+
+export class SignupComponent {
+  signupForm!: FormGroup<SignupForm>;
+
+  constructor(
+    private router: Router,
+    private loginService: LoginService,
+    private toastrService: ToastrService
+  ) {
+    this.signupForm = new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      passwordConfirm: new FormControl('', [Validators.required, Validators.minLength(6)])
+    })
+  }
+
+  submit() {
+    this.loginService.login(this.signupForm.value.email, this.signupForm.value.password).subscribe({
+      next: () => this.toastrService.success("Login realizado com sucesso!"),
+      error: () => this.toastrService.error("Erro inesperado! Tente novamente mais tarde.")
+    })
+  }
+
+  navigate() {
+    this.router.navigate(['/login'])
+  }
+}
